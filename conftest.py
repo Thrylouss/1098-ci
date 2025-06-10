3e5161d09bb3ac28ba23945a586c65fd568af502
Thrylouss <shoxrux.shorustamov@gmail.com>
2025-06-10 17:54:52 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,8 +1,16 @@
 from selenium import webdriver
 import pytest
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def driver_chrome():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(options=options)
     driver.get("https://my.proweb.uz/log-in")
+
     yield driver
+
+    driver.quit()
